20971465074a3a0cb6d11affd431be7f27dc7e88
HOWAIDI MADALLAH ALSANQR <r3.mawid@gmail.com>
2026-07-18 13:48:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ EMAILS_MAP = {
     "د.عادل الحربي": "adilalharby@gmail.com",
     "بريده المطيري": "buraida990@gmail.com",
     "منى العتيبي": "muna@example.com",
-    "هويدي الصنقر": "hwidii@gmail.com",
+    "هويدي الصنقر": "halsanqer@moh.gov.sa",
     "المسؤول": "r3-mawid@gmail.com"
 }
 
